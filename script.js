2433bb474aa9cb858ad444c38fa915a0f07becf7
Ryan Kim <ryanhkim0418@gmail.com>
2025-09-12 22:47:00 -0400

    making forms work

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// coment
+// Form reading?
+
+const inputForm = document.querySelector("form");
+
+inputForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    const addressInput = document.getElementById("address");
+    const radiusInput = document.getElementById("radius");
+
+    addressValue = addressInput.value;
+    radiusValue = radiusInput.value;
+
+    console.log(addressValue + " " + radiusValue); 
+
+    inputForm.reset();
+})
